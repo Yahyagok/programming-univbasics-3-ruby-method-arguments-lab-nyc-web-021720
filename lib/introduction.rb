25fb1b82ac71057c264d556bb282bd7f970c7a46
Yahya  Gok <ygok17@hotmail.com>
2020-01-20 04:11:12 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,16 +5,16 @@
 
 def introduction(name)
 puts "Hi, my name is #{name}"
-end 
+end
 
 def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning in #{language}"
-end 
+end
 
 
 def introduction_with_language(name,language = "Ruby")
   puts "Hi, my name is #{name} and I am learning in #{language}"
-end 
+end
 
 
 
